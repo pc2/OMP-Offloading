77721146de8087df2e9693520da4a6d0865e9bfb
xwuupb <xinwu@mail.uni-paderborn.de>
2020-01-14 20:37:47 +0100

    Asynchronous offloading is not available in GCC, but available in Clang/LLVM

--- 03_taskwait/tests/taskwait_real_00.sh
+++ 03_taskwait/tests/taskwait_real_00.sh
@@ -4,5 +4,15 @@
 #CCS -g pc2-mitarbeiter
 #CCS --res=rset=1:ncpus=1:mem=4g:vmem=8g:gtx1080=2
 
+if [ 0 -eq 1 ]; then
+#
+# Asynchronous offloading is not available in GCC 9.2.0.
+#
 notImpld=$(../src/taskwait 2>&1 | grep "GOMP_OFFLOAD_async_run")
 [[ $notImpld =~ "unimplemented" ]]
+else
+#
+# Asynchronous offloading is     available in Clang/LLVM 9.0.1.
+#
+../src/taskwait
+fi
